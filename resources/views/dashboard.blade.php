a11a8aecf4a946f635681271181cd43f9026308a
idris lawal <idriseun222@gmail.com>
2022-12-25 15:49:36 +0100

    feat: added home page button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a classs="btn btn-primary" href="{{ route('home') }}">
+                <button class="btn btn-primary my-2">
+                < Go to home page
+                </button>
+            </a>
+
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <p><strong>Create a new post</strong></p>
